3ffa8a64b0fe52b06aeb224abffd2e968559a7fb
Roman Franchuk <franchukrom@gmail.com>
2013-10-19 19:50:54 +0300

    HTML + Logo

--- js/init.js
+++ js/init.js
@@ -3,20 +3,20 @@ $(document).ready(function () {
     var i = 0, max = em.TITLES.length,
         textarea;
     
-    em.fuzzyset = em.Fuzzyset() 
+    em.fuzzyset = em.Fuzzyset(); 
     
     for (; i < max; i += 1) {
         em.fuzzyset.add(em.TITLES[i]);
     }
     // initialization
-    textarea = em.CodeMirror.fromTextArea($("#textdata")[0], {
-        mode: "text/html",
-        lineNumbers: true
-    });
-    textarea.on("change", function (e) {
-        var data = em.validator.getData(e.getValue());
-        em.validator.showData(data);
-    });
+    //textarea = em.CodeMirror.fromTextArea($("#textdata")[0], {
+    //    mode: "text/html",
+    //    lineNumbers: true
+    //});
+    //textarea.on("change", function (e) {
+    //    var data = em.validator.getData(e.getValue());
+    //    em.validator.showData(data);
+    //});
     
     $("#download-svg").click(function () {
         em.downloader.downloadSVG("test");
@@ -24,5 +24,22 @@ $(document).ready(function () {
     
     $("#download-png").click(function () {
         em.downloader.downloadPNG("test");
+    });
+    
+    $(".tab").click(function() {
+        $(".activeTab").removeClass("activeTab");
+        $(this).addClass("activeTab");
+    });
+    
+    $("#upload-tab").click(function() {
+       $("#upload").css("display", "block");
+       $("#datainput").css("display", "none");
+       $("#start").css("display", "none");
+    });
+    
+    $("#input-tab").click(function() {
+       $("#upload").css("display", "none");
+       $("#datainput").css("display", "block");
+       $("#start").css("display", "none");
     });
 });
